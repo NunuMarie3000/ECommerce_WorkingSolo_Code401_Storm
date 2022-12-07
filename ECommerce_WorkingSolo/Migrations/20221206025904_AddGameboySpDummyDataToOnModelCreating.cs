497660617a6e985cac213c0301fefb3dfdd77a88
Storm O'Bryant <vmarie1997@gmail.com>
2022-12-06 22:16:57 -0600

    final gitignore fix

---
+++ ECommerce_WorkingSolo/Migrations/20221206025904_AddGameboySpDummyDataToOnModelCreating.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceWorkingSolo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameboySpDummyDataToOnModelCreating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Condition", "Description", "ImagePath", "Name", "Price", "Rating" },
+                values: new object[] { 88, 8, 1, "This is a mint condition Gameboy Advance SP Gaming console. Sold with no cartridges", "/images/gaming_consoles/gameboyadvancesp_gaming_console.jpg", "Gameboy Advance Sp", 176.82m, 4 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 88);
+        }
+    }
+}
---
+++ ECommerce_WorkingSolo/Migrations/20221206173931_SlightUpdateBeforeAddingAdminAccounts.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceWorkingSolo.Migrations
+{
+    /// <inheritdoc />
+    public partial class SlightUpdateBeforeAddingAdminAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Condition", "Description", "ImagePath", "Name", "Price", "Rating" },
+                values: new object[] { 76, 8, 4, "This is a mint condition Gameboy Advance SP Gaming console. Sold with no cartridges", "/images/gaming_consoles/gameboyadvancesp_gaming_console.jpg", "Gameboy Advance Sp", 176.82m, 4 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 76);
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Condition", "Description", "ImagePath", "Name", "Price", "Rating" },
+                values: new object[] { 20, 8, 1, "This is a mint condition Gameboy Advance SP Gaming console. Sold with no cartridges", "/images/gaming_consoles/gameboyadvancesp_gaming_console.jpg", "Gameboy Advance Sp", 176.82m, 4 });
+        }
+    }
+}
